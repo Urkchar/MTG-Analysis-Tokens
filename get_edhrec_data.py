b27956b60f261d453f00623e07824deb7025e2d2
Riley Wittwer <urkchar@gmail.com>
2026-03-23 16:15:19 -0500

    Add logging and retry for 404

--- get_edhrec_data.py
+++ get_edhrec_data.py
@@ -5,9 +5,12 @@ import json
 from pathlib import Path
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
+import logging
 
 from scryfall import Scryfall
 from token_bucket import HTTPClient
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 
 class EDHRec:
@@ -48,26 +51,48 @@ class EDHRec:
     
     def build_next_js_url(self, route: str, identifier: str) -> str:
         """identifier is a formatted commander name or a URL hash"""
-        url = f"{self.base_next_js_url}/{self.build_id}/{route}/{identifier}.json"
+        url = (f"{self.base_next_js_url}/{self.build_id}/{route}/{identifier}"
+                ".json")
         return url
     
+    def _get_retry_once(self, route: str, identifier: str):
+        url = self.build_next_js_url(route, identifier)
+
+        # build_id could be stale. If we get a 404, refresh the build_id
+        # and try again once.
+        try:
+            resp = self._http_client.get(url)
+            return resp
+        except requests.exceptions.HTTPError as e:
+            logging.warning("HTTPError in _get_retry_once.")
+            if e.response is not None and e.response.status_code == 404:
+                logging.debug("Status code was 404. Retrying once...")
+                self.set_build_id()
+                # Rebuild the URL with the new build_id
+                url = self.build_next_js_url(route, identifier)
+                try:
+                    resp = self._http_client.get(url)
+                    return resp
+                except requests.exceptions.HTTPError as e2:
+                    if (e2.response is not None
+                        and e2.response.status_code == 404):
+                        logging.warning(
+                            "Second retry returned 404, returning None.")
+                        return None
+                    raise
+            else:
+                logging.error("Status code was not 404. Raising.")
+                raise
+    
     def get_decks(self, commander: str):
-        url = self.build_next_js_url("decks", commander)
-        resp = self._http_client.get(url)
+        resp = self._get_retry_once("decks", commander)
         data = resp.json()["pageProps"]["data"]["table"]
         return data
     
     def get_deck(self, url_hash: str):
-        url = self.build_next_js_url("deckpreview", url_hash)
-        try:
-            resp = self._http_client.get(url)
-        except requests.exceptions.HTTPError as e:
-            if e.response.status_code == 404:
-                self.set_build_id()
-                return None
-        if not resp:
-            return None
-        data = resp.json()["pageProps"]["data"]["panels"]["deckinfo"]["deck_preview"]
+        resp = self._get_retry_once("deckpreview", url_hash)
+        data = resp.json()["pageProps"]["data"]
+        deck_preview = data["panels"]["deckinfo"]["deck_preview"]
         keep = {
             "cedh",
             "coloridentity",
@@ -79,7 +104,7 @@ class EDHRec:
             "urlhash",
             "deck"
         }
-        slim = {k: v for k, v in data.items() if k in keep}
+        slim = {k: v for k, v in deck_preview.items() if k in keep}
         return slim
 
     def save_decks(self, commander: str):
@@ -99,11 +124,13 @@ class EDHRec:
 
     def count_decks(self, commander: str) -> int:
         decks = self.get_decks(commander)
-        return len(decks)   # All decks for all commanders: 9,283,036. Not including flavor names: 8,314,401
+        return len(decks)   # All decks for all commanders: 9,283,036.
+                            # Not including flavor names: 8,314,401
 
 
 def format_commander_name(name: str) -> str:
-    # EDHRec formats commander names in URLs by lowercasing and replacing non-alphanumerics with hyphens
+    # EDHRec formats commander names in URLs by lowercasing and replacing
+    # non-alphanumerics with hyphens
     formatted = name.lower()
     formatted = formatted.replace("'", "")
     formatted = re.sub(r"[^a-z0-9]+", "-", formatted)
@@ -124,10 +151,20 @@ def get_invalid_commanders() -> set:
     invalid_commanders = set()
     scryfall_client = Scryfall()
 
-    # Some commanders have a flavor name, but are the same card ("Mina Harker" == "Thalia, Guardian of Thraben")
-    cards = scryfall_client.search(q_has="flavor_name", q_prints=">1", q_is="commander")
-    assert len(cards) == 126
-    flavor_names = [card["flavor_name"] if "flavor_name" in card else card["card_faces"][0]["flavor_name"] for card in cards]
+    # Some commanders have a flavor name, but are the same card
+    # ("Mina Harker" == "Thalia, Guardian of Thraben")
+    cards = scryfall_client.search(
+        q_has="flavor_name",
+        q_prints=">1",
+        q_is="commander")
+
+    flavor_names = []
+    for card in cards:
+        if "flavor_name" in card:
+            flavor_names.append(card["flavor_name"])
+        else:
+            flavor_names.append(card["card_faces"][0]["flavor_name"])
+
     invalid_commanders.update(flavor_names)
 
     # Some commanders have in-universe versions, but not flavor names
@@ -138,10 +175,17 @@ def get_invalid_commanders() -> set:
     # Themberchaud has an in-universe version, but its name is the same.
     invalid_commanders.remove("Themberchaud")
 
-    # Commanders from Through the Omenpaths have no flavor name, but are the same mechanically as their orginal version from Marvel's Spider-Man
+    # Commanders from Through the Omenpaths have no flavor name, but are the
+    # same mechanically as their orginal version from Marvel's Spider-Man
     cards = scryfall_client.search(q_set="om1", q_is="commander")
-    assert len(cards) == 79
-    names = [card["printed_name"] if "printed_name" in card else card["card_faces"][0]["printed_name"] for card in cards]
+
+    names = []
+    for card in cards:
+        if "printed_name" in card:
+            names.append(card["printed_name"])
+        else:
+            names.append(card["card_faces"][0]["printed_name"])
+
     invalid_commanders.update(names)
 
     return {format_commander_name(name) for name in invalid_commanders}
@@ -179,7 +223,7 @@ def get_commanders() -> list:
 
 def main():
     commanders = get_commanders()
-    print(f"Found {len(commanders)} unique commanders with decks on EDHRec.")
+    logging.info(f"Found {len(commanders)} unique commanders on EDHRec.")
 
     client = EDHRec(rate_per_sec=2)
 
